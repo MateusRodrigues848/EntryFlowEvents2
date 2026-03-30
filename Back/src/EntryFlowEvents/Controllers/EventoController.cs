31ce76281ca47d098fbf913281e56f2a3c31474e
Mateus_Rodrigues <mateus.rodrigues64@outlook.com>
2026-03-30 16:28:59 -0300

    First Commit
    criação do projeto Configuração inicial do controller e inicio de configuração do Models

---
+++ Back/src/EntryFlowEvents/Controllers/EventoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EntryFlowEvents.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Exemplo de Get";
+        }
+
+        [HttpPost]
+        public string post()
+        {
+            return "Exemplo de post";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Exemplo de Put";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Exemplo de Delete";
+        }
+    }
+}
---
+++ Back/src/EntryFlowEvents/Models/Evento.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntryFlowEvents.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+
+        public int Local { get; set; }
+
+        public int DataEvento  { get; set; }
+        
+        public int  { get; set; }
+    }
+}
